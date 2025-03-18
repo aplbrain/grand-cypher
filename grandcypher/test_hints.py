7d07d11471ac16d4f70ee2d37c1d7ecbee2eb77d
Jordan Matelsky <j6k4m8@gmail.com>
2025-03-18 18:30:17 -0400

    feat: Add test for multiple nodes with hints in graph queries

--- grandcypher/test_hints.py
+++ grandcypher/test_hints.py
@@ -89,3 +89,30 @@ class TestHints:
         """
         res = gc.run(qry, hints=hints)
         assert all(r == 1 for r in res["A"])
+
+    @pytest.mark.parametrize("graph_type", ACCEPTED_GRAPH_TYPES)
+    def test_multiple_nodes_hinted(self, graph_type):
+        """
+        Test the case where multiple nodes are hinted
+        """
+        host = graph_type()
+        host.add_node(1, name="Home")
+        host.add_node(2, name="Work")
+        host.add_node(3, name="School")
+        host.add_node(4, name="Library")
+        host.add_node(5, name="Park")
+        host.add_edge(1, 2)
+        host.add_edge(1, 3)
+        host.add_edge(2, 4)
+        host.add_edge(3, 4)
+        host.add_edge(4, 5)
+        host.add_edge(5, 1)
+
+        qry = """
+        MATCH (A)-[r]->(B)
+        RETURN A.name, B.name
+        """
+
+        gc = GrandCypher(host)
+        res = gc.run(qry, hints=[{"A": 1, "B": 2}, {"A": 3, "B": 4}])
+        assert res == {"A.name": ["Home", "School"], "B.name": ["Work", "Library"]}
